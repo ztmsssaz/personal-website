b5614d3a2d00be48046fd3c24e56f4d7db920438
unknown <zarei.mrabolfazl@gmail.com>
2024-08-14 19:46:09 +0330

    fix a bug

--- src/layout/header/index.tsx
+++ src/layout/header/index.tsx
@@ -1,43 +1,45 @@
 import {useEffect} from 'react'
 import {Github, Linkedin} from '../../components/icons/icons'
+import {Style} from './style'
 
 function Header() {
   useEffect(() => {
     window.addEventListener('scroll', () => {
       if (window.scrollY > 150) {
         document.getElementById('header')?.classList.add('hiddenHeader')
-        console.log('ok')
-      } else {
+      } else if (window.scrollY < 300) {
         document.getElementById('header')?.classList.remove('hiddenHeader')
       }
     })
   }, [])
 
   return (
-    <header
-      id='header'
-      className='z-10 py-2 opacity-85 fixed w-full top-0 right-0 font-bold bg-white text-slate-950 dark:text-white dark:bg-black'
-    >
-      <div className='container'>
-        <div className='flex justify-between'>
-          <div>
-            <img className='mx-auto' src='./logo192.png' width='50px' />
+    <Style>
+      <header
+        id='header'
+        className='z-10 py-2 opacity-85 fixed w-full top-0 right-0 font-bold bg-white text-slate-950 dark:text-white dark:bg-black'
+      >
+        <div className='container'>
+          <div className='flex justify-between'>
+            <div>
+              <img className='mx-auto' src='./logo192.png' width='50px' />
+            </div>
+            <ul className='flex justify-center items-center'>
+              <li className='mx-2'>
+                <a className='hover:cursor-pointer' href='https://github.com/ztmsssaz' target='_blank'>
+                  <Github size='28' className='border rounded ' />
+                </a>
+              </li>
+              <li className='mx-2'>
+                <a className='hover:cursor-pointer' href='https://www.linkedin.com/in/ztmsssaz/' target='_blank'>
+                  <Linkedin size='30' className='border rounded' />
+                </a>
+              </li>
+            </ul>
           </div>
-          <ul className='flex justify-center items-center'>
-            <li className='mx-2'>
-              <a className='hover:cursor-pointer' href='https://github.com/ztmsssaz' target='_blank'>
-                <Github size='28' className='border rounded ' />
-              </a>
-            </li>
-            <li className='mx-2'>
-              <a className='hover:cursor-pointer' href='https://www.linkedin.com/in/ztmsssaz/' target='_blank'>
-                <Linkedin size='30' className='border rounded' />
-              </a>
-            </li>
-          </ul>
         </div>
-      </div>
-    </header>
+      </header>
+    </Style>
   )
 }
 
--- src/layout/header/style.js
+++ src/layout/header/style.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const Style = styled.div`
   header {
-    transition: all 0.4s ease-in-out;
+    transition: all 0.3s ease-in-out;
     top: 0;
     &.hiddenHeader {
       top: -250px;
